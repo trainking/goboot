c11c4c7627a573a8fb9c4cd7a412c88df4ed3311
trainking <yefulincode@gmail.com>
2023-03-26 22:44:45 +0800

    fix: 服务端增加sll验证

--- pkg/boot/service.go
+++ pkg/boot/service.go
@@ -1,11 +1,15 @@
 package boot
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"io/ioutil"
 	"net"
 
 	"github.com/trainking/goboot/pkg/etcdx"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 // BaseService service的基础服务
@@ -42,7 +46,47 @@ func (s *BaseService) Init() error {
 		return err
 	}
 
-	s.GrpcServer = grpc.NewServer()
+	// 根据配置启用验证
+	switch s.Config.GetString(fmt.Sprintf("%s.Authentication.Mode", s.Name)) {
+	case "TLS":
+		// 加载TLS证书
+		cert, err := ioutil.ReadFile(s.Config.GetString(fmt.Sprintf("%s.Authentication.ServerCrt", s.Name)))
+		if err != nil {
+			return err
+		}
+		// 加载TLS密钥
+		key, err := ioutil.ReadFile(s.Config.GetString(fmt.Sprintf("%s.Authentication.ServerKey", s.Name)))
+		if err != nil {
+			return err
+		}
+
+		// 加载CA证书
+		ca, err := ioutil.ReadFile(s.Config.GetString(fmt.Sprintf("%s.Authentication.CaCrt", s.Name)))
+		if err != nil {
+			return err
+		}
+
+		// 创建证书池
+		certPool := x509.NewCertPool()
+		certPool.AppendCertsFromPEM(ca)
+
+		// 创建TLS配置
+		tlsConfig := &tls.Config{
+			Certificates: []tls.Certificate{
+				{
+					Certificate: [][]byte{cert},
+					PrivateKey:  key,
+				},
+			},
+			ClientCAs:  certPool,
+			ClientAuth: tls.RequireAndVerifyClientCert,
+		}
+
+		s.GrpcServer = grpc.NewServer(grpc.Creds(credentials.NewTLS(tlsConfig)))
+	default:
+		s.GrpcServer = grpc.NewServer()
+	}
+
 	err = s.serviceManager.Register(s.Addr)
 
 	return err
